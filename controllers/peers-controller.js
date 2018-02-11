54708a8820c96557efa62e3191f91330a186cf53
Simeon Kotashki <skotashki@yahoo.co.uk>
2018-02-11 23:32:23 +0200

    TODO note added

--- controllers/peers-controller.js
+++ controllers/peers-controller.js
@@ -15,6 +15,7 @@ module.exports = {
 
         if(urlValidator(peerUrl)) {
 
+            // TODO: could add funcitonality to check if 'peerName' is unique.
             var peerName = req.query.name;
 
             peer = new Peer(peerName,peerUrl);
