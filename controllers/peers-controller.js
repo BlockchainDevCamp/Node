82292f3cbefcf7a891ba90f65384d5d0043c213b
Simeon Kotashki <skotashki@yahoo.co.uk>
2018-02-11 23:24:52 +0200

    peer get and add functionalities implemented along with parameter validation for url

--- controllers/peers-controller.js
+++ controllers/peers-controller.js
@@ -1,10 +1,38 @@
+const ipRegex = require('ip-port-regex');
+const Node = require('../index');
+const Peer = require('../modules/Peer');
+
+
 module.exports = {
     getPeers: (req, res) => {
-        res.send("GET Peers");
-        // TODO
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(Node.peers));
     },
+    // post request with 2 parameters (name,url)
     postPeers: (req, res) => {
-        res.send("POST Peers");
-        // TODO
-    }
+
+    var peerUrl  = req.query.url;
+
+        if(urlValidator(peerUrl)) {
+
+            var peerName = req.query.name;
+
+            peer = new Peer(peerName,peerUrl);
+
+            Node.peers.push(peer);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).json({ message: 'Peer has been added',
+                                    peers: Node.peers} );
+
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400).json({ error: 'Wrong IP format' });
+        }
+
+    },
+};
+
+// method for validating URL format
+function urlValidator (ip) {
+    return ipRegex({exact: true}).test(ip)
 }
