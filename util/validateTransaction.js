7dba7304a64551463537fdc4fcb0e86bee37604b
frizeroBG <kovni@abv.bg>
2018-02-16 22:43:45 +0200

    upgrade validateAllTransactions.js

--- util/validateTransaction.js
+++ util/validateTransaction.js
@@ -1,42 +1,48 @@
 const node = require("../index");
 const hash = require('hash.js');
 
-function validateTransaction(transaction, block) {
+function validateTransaction(transaction) {
     // check if transaction is successfull
-    if(transaction.paid === false){
+    if (transaction.paid === false) {
         return true;
     }
-    
+
     // check is signature is correct TODO
 
-    // check if minedIndexBLock is correct
-    if(transaction.minedInBlockIndex !== block.index){
-        return false;
-    }
-
     // check if transaction hash is correct
-    if(!checkIfTransactionHashIsValid){
+    if (!checkIfTransactionHashIsValid) {
         return false;
     }
-
 
     // check if from address has enough money to send
-    if(node.balnances.get(transaction.senderPubKey) < transaction.fee + transaction.value){
+    if (node.balnances.get(transaction.from) < transaction.fee + transaction.value) {
         return false;
     }
 
-    // if all checks are correct update node balances
-    //updateNodeBalances(transaction);
-
 
     return true;
-}
 
-function checkIfTransactionHashIsValid(transaction) {
 
-    // TODO
+    function checkIfTransactionHashIsValid(transaction) {
 
-    return true;
+        let transactionHash = hash(
+            transaction.from,
+            transaction.to,
+            transaction.value,
+            transaction.senderPubKey,
+            transaction.senderSignature,
+            transaction.dateReceived,
+            transaction.minedInBlockIndex,
+            transaction.paid,
+            transaction.fee
+        );
+
+        if (transactionHash !== transaction.transactionHash){
+            return false;
+        }
+
+        return true;
+    }
 }
 
 module.exports = validateTransaction;
