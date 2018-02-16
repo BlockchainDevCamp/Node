4e685229b7dfd5f632c31df9b99dae20369dd9f2
karabinata <karabinata1992@gmail.com>
2018-02-16 02:21:34 +0200

    Get transaction function added

--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -1,7 +1,26 @@
 module.exports = {
     getTransaction: (req, res) => {
-        res.send("GET Transactions");
-        // TODO
+    const tranHash = req.params['transactionHash'];
+    const transactions = node.transactions;
+    let tranExists = false;
+    let transaction;
+
+    for (var i = 0; i < transactions; i++) {
+        if (transactions[i].transactionHash === tranHash) {
+            tranExists = true;
+            transaction = transactions[i];
+            break;
+        }
+    }
+
+    if (tranExists) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(transaction));
+    }
+    else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ "Error": "Invalid transaction hash" }));
+    }
     },
     postTransaction: (req, res) => {
         res.send("POST Transactions");
