d617f756a20af3eb9d2a982b3e4304f02236ee4d
frizeroBG <kovni@abv.bg>
2018-03-03 14:19:16 +0200

    implement mine-controller.js

--- controllers/mine-controller.js
+++ controllers/mine-controller.js
@@ -1,14 +1,78 @@
+const node = require('../index');
+const hash = require('hash.js');
+const Block = require('../modules/Block');
+const validateBlock = require('../util/validateBlock');
+
 module.exports = {
     startMine: (req, res) => {
+        let minerAddres = req.body.address;
+
+        let lastBlock = node.blocks[node.blocks - 1];
+
+        let blockDataHash = hash(lastBlock.index, node.pendingTransactions, node.difficulty, lastBlock.blockHash, minerAddres);
+
+        node.miningJobs.set(minerAddres, {
+            index: lastBlock.index,
+            transactions: node.pendingTransactions,
+            difficulty: node.difficulty,
+            prevBlockHash: lastBlock.blockHash,
+            blockDataHash: blockDataHash
+        });
+
+        let responseObj = {
+            index: lastBlock.index,
+            transactionsIncluded: node.pendingTransactions.length,
+            expectedReward: 10,
+            difficulty: node.difficulty,
+            blockDataHash: blockDataHash
+        }
+
+        res.setHeader('Content-Type', 'application/json');
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.send("POST mine");
-        // TODO
+        res.send(JSON.stringify(responseObj));
     },
-    submitBlock: (req, res) => {
+    submitBlock: async (req, res) => {
+        let index = req.body.index;
+        let nonce = req.body.nonce;
+        let dataCreated = req.body.dataCreated;
+        let blockHashMiner = req.body.blockHash;
+        let minerAddres = req.body.minerAddress;
+
+        let minerJob = node.miningJobs(minerAddres)
+
+        let lastBlock = node.blocks[node.blocks - 1];
+
+        let blockHash = hash(lastBlock.index, node.pendingTransactions.length, node.difficulty, minerJob.blockDataHash, nonce, dataCreated);
+
+        let isBlockValid = await validateBlock(newBlock);
+
+
+        if (blockHash.startsWith("0".repeat(node.difficulty)) && blockHash === blockHashMiner && index === node.blocks.length && blockHash === blockHashMiner && isBlockValid) {
+            let newBlock = new Block(index, minerJob.transactions, node.difficulty, lastBlock.blockHash, minerAddres, minerJob.blockDataHash, blockHash, nonce, dataCreated);
+
+            node.blocks.push(newBlock);
+
+            let responseObj = {
+                "status": "accepted",
+                "message": "Block accepted, expected reward: 10 coins"
+            }
+
+            res.setHeader('Content-Type', 'application/json');
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.send(JSON.stringify(responseObj));
+            return;
+        }
+
+        let responseObj = {
+            "status": "error"
+        }
+        res.setHeader('Content-Type', 'application/json');
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.send("POST submit Block");
-        // TODO
+        res.send(JSON.stringify(responseObj));
+
+
     }
 }
